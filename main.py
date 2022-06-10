8aab217e33ac887fb8840f4ff9f4470b77aa3a12
NTEZIRYAYO Celestin <97307891+Celesin@users.noreply.github.com>
2022-06-10 10:54:20 +0200

    new updates in pushed branch

--- main.py
+++ main.py
@@ -29,3 +29,23 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+      
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def myadd(x, y):
+    return x + y
+print(myadd(3,4)
+print(myadd(73,4)
+print(myadd(3,42)
+     def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint. 
